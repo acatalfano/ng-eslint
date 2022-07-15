9470a85faa984e6b0287b0b6d9f84b0d1a10862c
Adam Catalfano <acatalfano@gmail.com>
2022-07-14 22:18:39 -0400

    fix tsconfigs and jest configs post-ng14 upgrade

--- jest.config.js
+++ jest.config.js
@@ -4,33 +4,14 @@ module.exports = {
     bail: 1,
     clearMocks: false,
     collectCoverage: false,
-    coverageDirectory: 'coverage',
+    coverageDirectory: './coverage',
     coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
-
-    globals: {
-        'ts-jest': {
-            tsConfig: '<rootDir>/tsconfig.spec.json',
-            isolatedModules: true,
-            stringifyContentPathRegex: '\\.html$',
-            astTransformers: ['jest-preset-angular/InlineHtmlStripStylesTransformer.js']
-        }
-    },
-    moduleFileExtensions: ['ts', 'html', 'json', 'js'],
-    modulePathIgnorePatterns: ['<rootDir>/node_modules/.*'],
-    preset: 'jest-preset-angular',
-    roots: ['projects/lib/src', 'projects/test-app/src'],
+    roots: ['projects/lib/src', 'projects/showcase/src'],
     setupFilesAfterEnv: ['./setup-jest.ts'],
     snapshotSerializers: [
-        'jest-preset-angular/AngularNoNgAttributesSnapshotSerializer.js',
-        'jest-preset-angular/AngularSnapshotSerializer.js',
-        'jest-preset-angular/HTMLCommentSerializer.js'
+        'jest-preset-angular/build/serializers/no-ng-attributes',
+        'jest-preset-angular/build/serializers/ng-snapshot',
+        'jest-preset-angular/build/serializers/html-comment'
     ],
-    testEnvironment: 'jsdom',
-    testMatch: ['**/?(*.)+(spec).[tj]s?(x)'],
-    testPathIgnorePatterns: ['<rootDir>/node_modules'],
-    transform: {
-        [`(${esModules}).+\\.ts$`]: 'babel-jest',
-        '^.+\\.(ts|html)$': 'ts-jest'
-    },
-    transformIgnorePatterns: [`/node_modules/(?!${esModules})`]
+    testMatch: ['**/?(*.)+(spec).[tj]s?(x)']
 };
